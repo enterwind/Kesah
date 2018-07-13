cba4809167764b2abc7e41acef9a175b1f3d5757
Noviyanto Rahmadi <noviyantorahmadi@gmail.com>
2018-07-13 08:53:31 +0800

    Update

---
+++ app/src/main/java/vay/enterwind/kesah/activity/linimasa/LinimasaActivity.java
@@ -0,0 +1,13 @@
+package vay.enterwind.kesah.activity.linimasa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinimasaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linimasa);
+    }
+}
